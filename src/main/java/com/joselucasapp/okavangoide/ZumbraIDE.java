c9f4f1432fe2614cb801ab43a8ec6299f22f13a1
José Lucas <josluc2016@gmail.com>
2025-05-12 12:30:59 -0300

    fix: screen positions

--- src/main/java/com/joselucasapp/okavangoide/ZumbraIDE.java
+++ src/main/java/com/joselucasapp/okavangoide/ZumbraIDE.java
@@ -19,7 +19,7 @@ public class ZumbraIDE extends Application{
     @Override
     public void start(Stage stage){
         SelectFile selectFile = new SelectFile();
-        
+
         TextArea editor = new TextArea();
         editor.setFont(Font.font("Fira code", 16));
         editor.setMinWidth(800);
@@ -59,13 +59,25 @@ public class ZumbraIDE extends Application{
             }
         });
 
+        Button openFile = new Button("File");
+        openFile.getStyleClass().add("run-button");
+
+        openFile.setOnAction(e->{
+            selectFile.start(stage);
+        });
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        HBox top_bar_menu = new HBox(openFile, spacer);
+        top_bar_menu.setAlignment(Pos.CENTER_LEFT);
+        top_bar_menu.setPadding(new Insets(10));
+
         Text outputText = new Text("Output: ");
         outputText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         outputText.getStyleClass().add("output-text");
         outputText.setFill(Color.rgb(248,248,242));
 
-        Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
+
         HBox layout_btn_msg = new HBox(outputText, spacer, runButton);
 
         layout_btn_msg.setAlignment(Pos.CENTER_LEFT);
@@ -74,22 +86,26 @@ public class ZumbraIDE extends Application{
         VBox layout_infos_output = new VBox(layout_btn_msg, output);
         HBox layout = new HBox(editor);
         HBox.setMargin(layout_infos_output, new Insets(0,0,0,20));
+        HBox.setHgrow(layout, Priority.ALWAYS);
         layout.getChildren().add(layout_infos_output);
 
         layout.setAlignment(Pos.CENTER);
         layout.setPadding(new Insets(10));
 
-        layout.setMinWidth(800);
+        layout.setMinWidth(700);
+        layout.setMinHeight(650);
         layout.getStyleClass().add("layout");
-        Scene scene = new Scene(layout, 1000, 600);
+
+        VBox okavangoIDE = new VBox(top_bar_menu, layout);
+        okavangoIDE.setMinWidth(800);
+        okavangoIDE.getStyleClass().add("ide");
+        Scene scene = new Scene(okavangoIDE, 1000, 600);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/resources/css/stylesheet.css")).toExternalForm());
 
         stage.setTitle("OkavangoIDE");
         stage.setScene(scene);
 
         stage.show();
-
-        selectFile.start(stage);
     }
 
 }
