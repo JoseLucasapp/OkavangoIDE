6db3e5bb4248e4b03830544d55e7e3f168ef498f
José Lucas <josluc2016@gmail.com>
2025-05-13 12:58:55 -0300

    fix: remove css files

--- src/main/java/com/joselucasapp/okavangoide/TextField.java
+++ src/main/java/com/joselucasapp/okavangoide/TextField.java
@@ -1,20 +1,21 @@
 package main.java.com.joselucasapp.okavangoide;
 
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class TextField {
-    public TextArea start(double minHeight, int fontSize, String fontFamily, String cssClass, Color rgb, Boolean editable){
+    public TextArea start(double minHeight, int fontSize, String fontFamily, Boolean editable){
         TextArea editor = new TextArea();
         editor.setFont(Font.font(fontFamily, fontSize));
         editor.setMinHeight(minHeight);
-        editor.getStyleClass().add(cssClass);
         editor.setEditable(editable);
-        editor.setBackground(new Background(new BackgroundFill(rgb, null, null)));
-
+        editor.setStyle(
+            "-fx-control-inner-background: #44475a;" +
+            "-fx-background-color: #282a36;" +
+            "-fx-text-fill: #f8f8f2;" +
+            "-fx-focus-color: transparent;" +
+            "-fx-faint-focus-color: transparent;"
+        );
         return editor;
     }
 }
--- src/main/java/com/joselucasapp/okavangoide/ZumbraIDE.java
+++ src/main/java/com/joselucasapp/okavangoide/ZumbraIDE.java
@@ -6,13 +6,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import main.java.com.joselucasapp.okavangoide.helpers.ScreenData;
-import java.util.Objects;
 
 public class ZumbraIDE extends Application{
     @Override
@@ -25,17 +20,31 @@ public class ZumbraIDE extends Application{
         double screenX = screenData.getData()[0];
         double screenY = screenData.getData()[1];
 
-        TextArea editor = textField.start(0.63 * screenY,16, "Fira code", "text-area", Color.rgb(68,71,90), true);
-        TextArea output = textField.start(0.2 * screenY, 16, "Fira code", "text-area", Color.rgb(68,71,90), false);
+        TextArea editor = textField.start(0.63 * screenY,16, "Fira code", true);
+        TextArea output = textField.start(0.2 * screenY, 16, "Fira code", false);
 
         Button runButton = new Button("Run");
-        runButton.getStyleClass().add("run-button");
+        runButton.setStyle(
+            "-fx-font-size: 16px;"+
+            "-fx-padding: 10;"+
+            "-fx-background-color: #282a36;"+
+            "-fx-text-fill: #f8f8f2;"+
+            "-fx-border-color: #44475a;"+
+            "-fx-border-width: 1px;"+
+            "-fx-border-style: solid;"+
+            "-fx-cursor: hand;"
+        );
         runButton.setMinWidth(100);
 
         runButton.setOnAction(e -> runCode.start(editor, output));
 
         Button openFile = new Button("File");
-        openFile.getStyleClass().add("file-btn");
+        openFile.setStyle(
+            "-fx-font-size: 16px;"+
+            "-fx-background-color: #282a36;"+
+            "-fx-text-fill: #f8f8f2;"+
+            "-fx-cursor: hand;"
+        );
 
 
         HBox.setHgrow(spacer, Priority.ALWAYS);
@@ -47,11 +56,6 @@ public class ZumbraIDE extends Application{
         top_bar_menu.setAlignment(Pos.CENTER);
         top_bar_menu.setMinWidth(screenX);
         top_bar_menu.setMinHeight(0.05 * screenY);
-
-        Text outputText = new Text("Output: ");
-        outputText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        outputText.getStyleClass().add("output-text");
-        outputText.setFill(Color.rgb(248,248,242));
 
         HBox layout_btn_msg = new HBox(runButton);
 
@@ -66,9 +70,11 @@ public class ZumbraIDE extends Application{
         layout.setPadding(new Insets(10));
         layout.setMinWidth(0.8 * screenX);
         layout.setMinHeight(0.95 * screenY);
-        layout.getStyleClass().add("layout");
+        layout.setStyle(
+            "-fx-background-color: #282a36;"
+        );
 
-        VBox lateral_menu = new VBox(outputText);
+        VBox lateral_menu = new VBox();
         lateral_menu.getStyleClass().add("lateral-menu");
         lateral_menu.setMinWidth(0.2 * screenX);
         lateral_menu.setMinHeight(screenY - 100);
@@ -78,10 +84,10 @@ public class ZumbraIDE extends Application{
 
         VBox okavangoIDE = new VBox(top_bar_menu, body);
         okavangoIDE.setMinWidth(screenX);
-        okavangoIDE.getStyleClass().add("ide");
+        okavangoIDE.setStyle(
+                "-fx-background-color: #282a36;"
+        );
         Scene scene = new Scene(okavangoIDE, screenX, screenY);
-
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/resources/css/stylesheet.css")).toExternalForm());
 
         stage.setTitle("OkavangoIDE");
         stage.setScene(scene);
